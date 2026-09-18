34268862fb952d307d7d2a1dec12956c67770cb8
AdrianoCarlo <Ramarolahycarlo11@gmail.com>
2026-09-18 10:13:25 +0200

    chore: initialisation du projet Spring Boot Fiscal-Track

---
+++ src/main/java/mg/dgi/fiscaltrack/FiscalTrackApplication.java
@@ -0,0 +1,13 @@
+package mg.dgi.fiscaltrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/dgi/fiscal_track/FiscalTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.dgi.fiscal_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
