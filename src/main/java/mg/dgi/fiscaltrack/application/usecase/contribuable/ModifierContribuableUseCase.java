78ada8abffec8bdb61d831bcb20199180426e0f1
AdrianoCarlo <Ramarolahycarlo11@gmail.com>
2026-09-24 14:45:35 +0200

    fix: patch strict sur ModifierContribuableUseCase (harmonisation avec ModifierTypeImpotUseCase)

--- src/main/java/mg/dgi/fiscaltrack/application/usecase/contribuable/ModifierContribuableUseCase.java
+++ src/main/java/mg/dgi/fiscaltrack/application/usecase/contribuable/ModifierContribuableUseCase.java
@@ -14,22 +14,52 @@ public class ModifierContribuableUseCase {
         this.repositoryPort = repositoryPort;
     }
 
+    /**
+     * Modifie un contribuable en appliquant un patch strict : seuls les
+     * champs non-null de donneesModifiees sont ecrits. Les champs null
+     * ne modifient pas la valeur existante. Aligne sur le comportement
+     * de ModifierTypeImpotUseCase.
+     */
     public Contribuable execute(String nif, Contribuable donneesModifiees) {
         Contribuable existant = repositoryPort.findByNif(nif)
                 .orElseThrow(() -> new ContribuableIntrouvableException(nif));
 
-        existant.setRaisonSociale(donneesModifiees.getRaisonSociale());
-        existant.setFormeJuridique(donneesModifiees.getFormeJuridique());
-        existant.setNomDirigeant(donneesModifiees.getNomDirigeant());
-        existant.setPrenomDirigeant(donneesModifiees.getPrenomDirigeant());
-        existant.setCinDirigeant(donneesModifiees.getCinDirigeant());
-        existant.setEmailContribuable(donneesModifiees.getEmailContribuable());
-        existant.setTelephoneContribuable(donneesModifiees.getTelephoneContribuable());
-        existant.setAdresseContribuable(donneesModifiees.getAdresseContribuable());
-        existant.setCommuneContribuable(donneesModifiees.getCommuneContribuable());
-        existant.setRegimeImposition(donneesModifiees.getRegimeImposition());
-        existant.setObligationComptable(donneesModifiees.getObligationComptable());
-        existant.setStatutActivite(donneesModifiees.getStatutActivite());
+        if (donneesModifiees.getRaisonSociale() != null) {
+            existant.setRaisonSociale(donneesModifiees.getRaisonSociale());
+        }
+        if (donneesModifiees.getFormeJuridique() != null) {
+            existant.setFormeJuridique(donneesModifiees.getFormeJuridique());
+        }
+        if (donneesModifiees.getNomDirigeant() != null) {
+            existant.setNomDirigeant(donneesModifiees.getNomDirigeant());
+        }
+        if (donneesModifiees.getPrenomDirigeant() != null) {
+            existant.setPrenomDirigeant(donneesModifiees.getPrenomDirigeant());
+        }
+        if (donneesModifiees.getCinDirigeant() != null) {
+            existant.setCinDirigeant(donneesModifiees.getCinDirigeant());
+        }
+        if (donneesModifiees.getEmailContribuable() != null) {
+            existant.setEmailContribuable(donneesModifiees.getEmailContribuable());
+        }
+        if (donneesModifiees.getTelephoneContribuable() != null) {
+            existant.setTelephoneContribuable(donneesModifiees.getTelephoneContribuable());
+        }
+        if (donneesModifiees.getAdresseContribuable() != null) {
+            existant.setAdresseContribuable(donneesModifiees.getAdresseContribuable());
+        }
+        if (donneesModifiees.getCommuneContribuable() != null) {
+            existant.setCommuneContribuable(donneesModifiees.getCommuneContribuable());
+        }
+        if (donneesModifiees.getRegimeImposition() != null) {
+            existant.setRegimeImposition(donneesModifiees.getRegimeImposition());
+        }
+        if (donneesModifiees.getObligationComptable() != null) {
+            existant.setObligationComptable(donneesModifiees.getObligationComptable());
+        }
+        if (donneesModifiees.getStatutActivite() != null) {
+            existant.setStatutActivite(donneesModifiees.getStatutActivite());
+        }
 
         return repositoryPort.save(existant);
     }
